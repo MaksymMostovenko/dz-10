5cf3f4163a3d6c39e348fc3f74f70ecd31a74101
MaksymMostovenko <blockmaksim@gmail.com>
2023-05-16 19:52:12 +0300

    Create TestAccount enum

---
+++ src/TestAccount.java
@@ -0,0 +1,29 @@
+public enum TestAccount {
+    ACCOUNT_ID_000("accountId000", 50, "USD"),
+    ACCOUNT_ID_001("accountId001", 50, "EUR"),
+    ACCOUNT_ID_002("accountId002", 50, "USD"),
+    ACCOUNT_ID_003("accountId003", 250, "HRV"),
+    ACCOUNT_ID_004("accountId004", 50, "USD");
+
+    private String id;
+    private int amount;
+    private String currency;
+
+    private TestAccount(String id, int amount, String currency) {
+        this.id = id;
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+}
